b52a96e2cfdac0d48d242529e771d6e224a1e6f6
Laurynas <lorenco@gmail.com>
2012-11-21 23:22:56 +0000

    added Estonian, Italian and Russian translations

---
+++ locales/et/text.js
@@ -0,0 +1,81 @@
+var defaultDateFormat = {
+ 'time' : '',
+ 'day' : 'd. NNN',
+ 'sd-title' : 'EE, d. MMM yyyy, H:mm',
+ 'months-short' : new Array('jaan', 'veebr', 'märts', 'apr', 'mai', 'juuni', 
+ 'juuli', 'aug', 'sept', 'okt', 'nov', 'dets'),
+ 'months' : new Array('jaanuar', 'veebruar', 'märts', 'aprill', 'mai', 'juuni', 
+ 'juuli', 'august', 'september', 'oktoober', 'november', 'detsember'),
+ 'weekdays-short' : new Array('P', 'E', 'T', 'K', 'N', 'R', 'L'),
+ 'weekdays' : new Array('pühapäev', 'esmaspäev', 'teisipäev', 'kolmapäev', 'neljapäev', 'reede', 'laupäev')
+}
+
+var defaultText = {
+
+ 'index-title' : 'Google\'i kalender',
+ 'description' : 'Google\'i kalendri sündmused kiirvalimisel.',
+ 'options-title' : 'Google\'i kalendri seaded',
+
+ 'refresh-cal' : 'Värskenda kalendreid',
+ 'options-account' : 'Konto',
+ 'options-share-session' : 'Brauseri seansi jagamine',
+ 'options-separate-signin' : 'Logi sisse teise kontoga:',
+ 'options-email' : 'E-post',
+ 'options-password' : 'Parool',
+ 'signin' : 'Logi sisse',
+ 'signout' : 'Logi välja',
+ 'options-signed' : 'Oled sisse logitud Google\' kontoga:',
+ 'options-user-email' : 'E-post:',
+
+ 'options-options' : 'Seaded',
+ 'options-language' : 'Keel',
+ 'options-lang-auto' : 'Automaatne',
+
+ 'options-1-min' : '1 minut',
+ 'options-5-min' : '5 minutit',
+ 'options-10-min' : '10 minutit',
+ 'options-15-min' : '15 minutit',
+ 'options-30-min' : '30 minutit',
+ 'options-1-h' : '1 tund',
+ 'options-5-h' : '5 tundi',
+
+ 'options-refresh-interval' : 'Värskendusintervall',
+ 'options-auto' : 'Automaatne',
+ 'options-time-zone' : 'Ajavöönd',
+ 'options-default-cal' : 'Vaikimisi kalender',
+ 'options-own-cals' : 'Oma kalendrid',
+ 'options-all-cals' : 'Kõik kalendrid',
+ 'options-selected-cals' : 'Valitud kalendrid',
+ 'options-calendars' : 'Kalendrid',
+ 'options-all-cal-note' : '* "Kõik kalendrid" hõlmab ka tellitud kalendreid.',
+ 'options-cals-to-use' : 'Kasutatavad kalendrid',
+ 'options-appearance' : 'Välimus',
+ 'options-bg-color' : 'Taustavärv',
+ 'options-font-color' : 'Teksti värv',
+ 'options-alt-font-color' : 'Vahelduv teksti värv (kogupäevasündmustele)',
+ 'options-mx-entries' : 'Näidatavate kirjete suurim arv',
+ 'options-past-events' : 'Kuvatakse kõik tänased sündmused (sh möödunud)',
+ 'options-end-time' : 'Kuvatakse lõppkellaaega',
+ 'options-clock-12h' : '12-tunnine kell',
+ 'options-date-format' : 'Kuupäevavorming, vt <a target="_blank" href="http://www.javascripttoolbox.com/lib/date/documentation.php">näiteid</a>',
+ 'options-title-date' : 'Kiirvalimise pealkirjavorming, vt <a target="_blank" href="http://www.javascripttoolbox.com/lib/date/documentation.php">näiteid</a>',
+ 'options-font-size' : 'Fondi suurus: ',
+ 'options-font-px' : 'pix',
+ 'options-wrap-lines' : 'Ürituste pealkirjade murdmine',
+ 'options-reset-prefs' : 'Lähtesta seaded',
+ 'options-footer' : 'Kõik ühendused Google\'i serveritesse käivad HTTPS-protokolliga. Parooli ei salvestata kuskil, seda kasutatakse üksnes esmakordsel sisselogimisel.',
+
+ 'options-login' : 'Logi sisse',
+ 'options-error' : 'Viga: ',
+ 'options-signing-in' : 'Sisselogimine...',
+
+ 'options-reset-confirm' : 'Kas soovid kindlasti seaded lähtestada?',
+ 'options-debug-data' : 'Silumisandmed',
+
+ 'view-unknown-error' : 'Tundmatu viga!',
+ 'view-click-signin' : 'Klõpsa sisselogimiseks...',
+ 'view-signin-prefs' : 'Palun logi sisse laienduse eelistuste lehel',
+ 'view-no-events' : 'Sündmusi pole'
+
+}
+updateTranslation();
